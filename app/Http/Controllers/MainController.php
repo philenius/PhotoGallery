699c34686177816269b95a06dedd48d09474ba97
philenius <philippus-perez@web.de>
2016-10-04 22:14:10 +0200

    fixed bugs in upload function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,8 +14,13 @@ use App\PhotoSubject;
 
 class MainController extends Controller
 {
-	private static $fileUploadDirectory = 'uploads';
+	private static $fileUploadDirectory = '';
 	private static $photosPerPage = 8;
+
+	public function __construct()
+	{
+		self::$fileUploadDirectory = public_path() . '/uploads';
+	}
 
     public function getIndex($page = NULL)
     {
@@ -32,7 +37,7 @@ class MainController extends Controller
     	}
 
     	$nextPage = false;
-    	if ($page < $totalPageCount)
+    	if ($page < floor($totalPageCount))
     	{
     		$nextPage = true;
     	}
@@ -40,12 +45,12 @@ class MainController extends Controller
 		$intervalStart = ($page-1) * self::$photosPerPage;
     	$photos = Photo::offset($intervalStart)->limit(self::$photosPerPage)->get();
     	$allPhotoSubjects = PhotoSubject::all();
+
     	return view('index')->with('photos', $photos)->with('photoSubjects', $allPhotoSubjects)->with('page', $page)->with('nextPage', $nextPage);
     }
 
     public function uploadPhoto(Request $request)
     {
-
 		$this->validate($request, [
 	        'photoTitle' => 'required',
 	        'photoLocation' => 'required',
@@ -53,38 +58,43 @@ class MainController extends Controller
 	        'photoSubject' => 'required|integer'
 	    ]);
 
-    	$photo = Input::file('photo');
-    	if (count($photo) == 1)
-    	{
-    		$mimeType = $photo->getMimeType();
-    		if ($mimeType == 'image/jpeg' || $mimeType == 'image/png')
-    		{
-    			$photoSubject = PhotoSubject::find($request->input('photoSubject'));
+		if ($request->hasFile('photo')) {
+	    	
+	    	$photo = Input::file('photo');
+	    	
+	    	if ($photo->isValid())
+	    	{
+	
+				$mimeType = $photo->getMimeType();
+				if ($mimeType == 'image/jpeg' || $mimeType == 'image/png')
+				{
+					$photoSubject = PhotoSubject::find($request->input('photoSubject'));
 
-    			$savedPhoto = Photo::create(['title' => $request->input('photoTitle'), 'location' => $request->input('photoLocation')]);
-    			$fileExtension = '.' . $photo->guessClientExtension();
-    			$originalFile = $savedPhoto->id . $fileExtension;
-    			$compressedFile = $savedPhoto->id . '_compressed' . $fileExtension;
-    			$thumbnailFile = $savedPhoto->id . '_thumb' . $fileExtension;
+					$savedPhoto = Photo::create(['title' => $request->input('photoTitle'), 'location' => $request->input('photoLocation')]);
+					$fileExtension = '.' . $photo->guessClientExtension();
+					$originalFile = $savedPhoto->id . $fileExtension;
+					$compressedFile = $savedPhoto->id . '_compressed' . $fileExtension;
+					$thumbnailFile = $savedPhoto->id . '_thumb' . $fileExtension;
 
-    			// Duplicate photo
-    			$photo->move(self::$fileUploadDirectory, $originalFile);
-    			copy(self::$fileUploadDirectory . '/' . $originalFile, self::$fileUploadDirectory . '/' . $compressedFile);
-    			copy(self::$fileUploadDirectory . '/' . $originalFile, self::$fileUploadDirectory . '/' . $thumbnailFile);
+					// Duplicate photo
+					$path = $photo->move(self::$fileUploadDirectory, $originalFile);
+					copy($path, self::$fileUploadDirectory . '/' . $compressedFile);
+					copy($path, self::$fileUploadDirectory . '/' . $thumbnailFile);
 
-    			// Resize photos
-    			exec('cd uploads/ && mogrify -resize x700 -quality 70 ' . $thumbnailFile);
-    			exec('cd uploads/ && mogrify -quality 70 ' . $compressedFile);
+					// Resize photos
+					exec('cd uploads/ && mogrify -resize x700 -quality 70 ' . $thumbnailFile);
+					exec('cd uploads/ && mogrify -quality 70 ' . $compressedFile);
 
-				$savedPhoto->file_name_original = $originalFile;
-    			$savedPhoto->file_name_compressed = $compressedFile;
-    			$savedPhoto->file_name_thumbnail = $thumbnailFile;
-    			$savedPhoto->save();
+					$savedPhoto->file_name_original = $originalFile;
+					$savedPhoto->file_name_compressed = $compressedFile;
+					$savedPhoto->file_name_thumbnail = $thumbnailFile;
+					$savedPhoto->save();
 
-    			$photoSubject->photos()->save($savedPhoto);
-    		}
-    	}
+					$photoSubject->photos()->save($savedPhoto);
+		   		}
+			}
 
+	   	}
     	return redirect()->route('index');
     }
 }
