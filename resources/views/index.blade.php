9955a2586b68ff3b5c39240b24f58c33ef90ab65
philenius <philippus-perez@web.de>
2016-10-04 22:14:20 +0200

    changed page title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Malaysia Summer Camp 2016</title>
+    <title>Kompaktprogramm Malaysia 2016</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
